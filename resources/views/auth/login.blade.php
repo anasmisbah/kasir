5af6285e469b4045377a05131313098c755e03c2
anasmisbah <anas.misbahuddin4869@gmail.com>
2020-03-14 08:33:43 +0800

    fix login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,94 +1,107 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
     @php
     use App\Application;
     $app = Application::first();
     @endphp
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Masuk | {{$app->nama}}</title>
-    <!-- Tell the browser to be responsive to screen width -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="/adminlte/plugins/fontawesome-free/css/all.min.css">
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-    <!-- icheck bootstrap -->
-    <link rel="stylesheet" href="/adminlte/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="./adminlte/dist/css/adminlte.min.css">
-    <link rel="shortcut icon" href="{{asset('/img/favico.png')}}" type="image/x-icon">
-    <!-- Google Font: Source Sans Pro -->
-    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <!-- App Desc -->
+    <meta name="description" content="Halaman Login" />
+    <meta name="author" content="tukangkode.id" />
+    <title>Login | {{$app->nama}}</title>
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="assets/favicon/favicon.ico" type="image/x-icon" />
+    <!-- Font -->
+    <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet" />
+    <!-- Icon -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css" />
+    <!-- CSS -->
+    <link rel="stylesheet" href="https://unpkg.com/@coreui/coreui@3.0.0-rc.0/dist/css/coreui.min.css">
+    <!--
+        [if lt IE 9]>
+            <script src="http://css3-mediaqueries-js.googlecode.com/files/css3-mediaqueries.js"></script>
+            <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]
+    -->
     <style>
-    .card{
-        border: 3px solid;
-        border-color: #6c757d;
-    }
+        html,
+        body {
+            height: 100%;
+        }
 
+        body {
+            font-family: 'Lato', sans-serif;
+            display: -ms-flexbox;
+            display: -webkit-box;
+            display: flex;
+            -ms-flex-align: center;
+            -ms-flex-pack: center;
+            -webkit-box-align: center;
+            align-items: center;
+            -webkit-box-pack: center;
+            justify-content: center;
+            padding-top: 40px;
+            padding-bottom: 40px;
+            background-color: rgb(235, 235, 235);
+        }
+
+        .form-signin {
+            width: 100%;
+            max-width: 330px;
+            padding: 15px;
+            margin: 0 auto;
+        }
+
+        .form-signin .form-control {
+            position: relative;
+            box-sizing: border-box;
+            height: auto;
+            padding: 10px;
+            font-size: 16px;
+        }
     </style>
 </head>
-
-<body class="hold-transition login-page">
-    <div class="login-box" style="width:300px">
-        <!-- /.login-logo -->
-        <div class="card">
-            <div class="card-body rounded">
-                    <div class="login-logo">
-                        <img height="30px" src="{{asset('/uploads/'.$app->logo)}}" alt="">
-                    </div>
-
-                <form action="{{ route('login') }}" method="post">
-                    @csrf
-                    <div class="input-group mb-3 mt-5">
-                        <input type="text" name="username" class="form-control" placeholder="Username">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-user"></span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="input-group mb-3">
-                        <input type="password" name="password" class="form-control" placeholder="Password">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-lock"></span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <!-- /.col -->
-                        <div class="col">
-                            <button type="submit" class="btn btn-secondary btn-block">LOGIN</button>
-                            <p class="text-center mt-5 mb-0" style="font-size:12px">&copy; {{ now()->year }} | Developed with <span style="color:#b71c1c">&hearts;</span> by </p>
-                        </div>
-                        <!-- /.col -->
-                    </div>
-                </form>
+<body class="c-app text-center">
+    <form class="form-signin" action="{{ route('login') }}" method="post">
+        @csrf
+        <img class="mb-5" src="{{asset('/uploads/'.$app->logo)}}" height="40px" alt="Nama Aplikasi">
+        <h2 class="mb-3 font-weight-normal" style="color: #000;">Silahkan login</h2>
+        <div class="input-group mb-3">
+            <input type="text" id="inputUser" class="form-control" name="username" placeholder="Username">
+            <div class="input-group-append">
+                <span class="input-group-text"><i class="fas fa-user"></i></span>
             </div>
-            <!-- /.login-card-body -->
         </div>
-    </div>
-    <!-- /.login-box -->
-
+        <div class="input-group mb-3">
+            <input type="password" id="inputPass" name="password" class="form-control" placeholder="Password" required>
+            <div class="input-group-append">
+                <span class="input-group-text"><i class="fas fa-lock"></i></span>
+            </div>
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">LOGIN</button>
+        <p class="mt-5 text-muted" style="font-size: 12px;">
+            &copy; 2020 | Developed with
+            <span style="color:#b71c1c"><i class="fas fa-heart"></i></span>
+            by
+            <a href="#"><img src="{{asset('img/logo-dev.png')}}" height="12px" alt="tukangkode.id"></a>
+        </p>
+    </form>
+    <!-- Script -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.15.0/umd/popper.min.js"></script>
+    <script src="https://unpkg.com/@coreui/coreui@3.0.0-rc.0/dist/js/coreui.min.js"></script>
     <!-- jQuery -->
     <script src="/adminlte/plugins/jquery/jquery.min.js"></script>
-    <!-- Bootstrap 4 -->
-    <script src="/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <!-- AdminLTE App -->
-    <script src="/adminlte/dist/js/adminlte.min.js"></script>
-
-</body>
-<script src="/adminlte/plugins/sweetalert.min.js"></script>
+    <script src="/adminlte/plugins/sweetalert.min.js"></script>
     <script>
         $(function(){
             var error = '{{ $errors->first() }}'
             if (error) {
                swal("Login Failed!", error, "error");
             }
-
         })
     </script>
+</body>
 </html>
